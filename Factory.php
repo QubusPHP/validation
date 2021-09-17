fef004ec9fa32b35823b5f89949d0ba09eb71134
Joshua Parker <josh@joshuaparker.blog>
2021-09-16 20:49:49 -0700

    Updated file headers.

--- Factory.php
+++ Factory.php
@@ -4,7 +4,7 @@
  * Qubus\Validation
  *
  * @link       https://github.com/QubusPHP/validation
- * @copyright  2020 Joshua Parker
+ * @copyright  2020 Joshua Parker <josh@joshuaparker.blog>
  * @license    https://opensource.org/licenses/mit-license.php MIT License
  *
  * @since      1.0.0
