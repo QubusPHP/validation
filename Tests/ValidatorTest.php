3eb63aa216278f135a1cfc953f4811f7b215e139
Joshua Parker <joshua@joshuaparker.dev>
2023-08-25 15:27:53 -0600

    Removed class header.

--- Tests/ValidatorTest.php
+++ Tests/ValidatorTest.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Qubus\Validation
- *
- * @link       https://github.com/QubusPHP/validation
- * @copyright  2020 Joshua Parker <josh@joshuaparker.blog>
- * @license    https://opensource.org/licenses/mit-license.php MIT License
- *
- * @since      1.0.0
- */
-
 declare(strict_types=1);
 
 namespace Qubus\Tests\Validation;
