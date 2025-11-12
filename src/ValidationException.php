b2288b4a240a0213de5236f9da25aa1417c3359b
Joshua Parker <joshua@joshuaparker.dev>
2025-11-12 13:31:55 -0800

    Fixed ValidationException.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -11,7 +11,7 @@ use Throwable;
 
 use function sprintf;
 
-class ValidationException extends Exception implements HttpException
+class ValidationException extends HttpException
 {
     public function __construct(
         protected UriInterface|string|null $uri = null,
@@ -20,43 +20,6 @@ class ValidationException extends Exception implements HttpException
         protected array $headers = [],
         protected $code = 0,
     ) {
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function __toString(): string
-    {
-        return static::class . sprintf(
-            ' %s in %s(%s)',
-            $this->message,
-            $this->file,
-            $this->line,
-        ) . "\n" . sprintf('%s', $this->getTraceAsString());
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getStatusCode(): int
-    {
-        return (int) $this->code;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getHeaders(): array
-    {
-        return $this->headers;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getUri(): UriInterface|string|null
-    {
-        return $this->uri;
+        parent::__construct($uri, $message, $previous, $headers, $code);
     }
 }
